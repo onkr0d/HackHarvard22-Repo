21c7b18c5d8e883d153b03fb27ef51ce42b3727d
onkr0d <90716666+onkr0d@users.noreply.github.com>
2022-10-16 03:29:38 -0400

    fix main squash

--- Frontend/music_app/lib/main.dart
+++ Frontend/music_app/lib/main.dart
@@ -16,90 +16,12 @@ class MusicApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, dwsfwfsß',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
     return MaterialApp(
-<<<<<<< HEAD
-      title: "Flutter layout demo",
-      home: Scaffold (
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: Column(
-          children: [
-            titleSection,
-          ],
-        ),
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-=======
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
->>>>>>> main
       ),
-    );
-  }
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -147,22 +69,14 @@ class _MyHomePageState extends State<MyHomePage> {
           // Our body will have to switch between Camera and Song pages.
           CameraPage(
             cameraDescription: _cameras.isEmpty ? null : _cameras,)
-          ),
+      ),
       theme: ThemeData(
         brightness: Brightness.light,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-<<<<<<< HEAD
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        // child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-=======
       themeMode: ThemeMode.dark,
->>>>>>> main
     );
   }
 }
