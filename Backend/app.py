00a625adb1dd9775e9051ddedc52c2d61a381842
onkr0d <90716666+onkr0d@users.noreply.github.com>
2022-10-16 02:27:46 -0400

    start backend in hopes of unifying all python code

---
+++ Backend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask import Flask, request, Response, jsonify, send_from_directory, abort
+import os
+from fer import FER
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/image', methods=['POST'])
+def receive_image():
+    print("received image")
+    print(request.files)
+
+
+
+    # Save the image to the filesystem
+    image = request.files['image']
+    image.save(os.path.join('images', image.filename))
+    return "received image"
+
+
+if __name__ == '__main__':
+    app.run()
