1e9852d2c859577c89da8ff5195c36bf5758fa3a
jas-li <jli3469@bu.edu>
2022-10-16 01:05:55 -0400

    fixed song query

--- spotifyPlay.py
+++ spotifyPlay.py
@@ -1,62 +1,70 @@
 import requests
 import base64
 import json
+import math
+import random
 
-# Step 1 - Authorization 
-url = "https://accounts.spotify.com/api/token"
-headers = {}
-data = {}
+def generateSongFromSearch(query):
+    # Step 1 - Authorization 
+    url = "https://accounts.spotify.com/api/token"
+    headers = {}
+    data = {}
 
-clientId = '1af2aaffd4ed412bba5b2c342b36d132'
-clientSecret = 'c10b92ba70cb40538e4188f75f432d21'
+    clientId = '1af2aaffd4ed412bba5b2c342b36d132'
+    clientSecret = 'c10b92ba70cb40538e4188f75f432d21'
 
-# Encode as Base64
-message = f"{clientId}:{clientSecret}"
-messageBytes = message.encode('ascii')
-base64Bytes = base64.b64encode(messageBytes)
-base64Message = base64Bytes.decode('ascii')
+    # Encode as Base64
+    message = f"{clientId}:{clientSecret}"
+    messageBytes = message.encode('ascii')
+    base64Bytes = base64.b64encode(messageBytes)
+    base64Message = base64Bytes.decode('ascii')
 
 
-headers['Authorization'] = f"Basic {base64Message}"
-data['grant_type'] = "client_credentials"
+    headers['Authorization'] = f"Basic {base64Message}"
+    data['grant_type'] = "client_credentials"
 
-r = requests.post(url, headers=headers, data=data)
+    r = requests.post(url, headers=headers, data=data)
 
-token = r.json()['access_token']
+    token = r.json()['access_token']
 
-# Step 2 - Use Access Token to query Spotify API and return a playlist ID
+    # Step 2 - Use Access Token to query Spotify API and return a playlist ID
 
-q = 'happy'
-searchUrl = f"https://api.spotify.com/v1/search?q={q}&type=playlist&market=US&limit=1&offset=4"
-headers = {
-    "Authorization": "Bearer " + token
-}
+    random.seed()
+    rand_song = random.randint(0,5) # randomize query 
 
-res = requests.get(url=searchUrl, headers=headers)
+    q = query
+    searchUrl = f"https://api.spotify.com/v1/search?q={q}&type=playlist&market=US&limit=1&offset={rand_song}"
+    headers = {
+        "Authorization": "Bearer " + token
+    }
 
-playlistId = res.json()['playlists']['items'][0]['id'] 
+    res = requests.get(url=searchUrl, headers=headers)
 
-# Step 3 - Use Token and playlist ID to return track information
+    playlistId = res.json()['playlists']['items'][0]['id'] 
 
-playlistUrl = f"https://api.spotify.com/v1/playlists/{playlistId}/tracks?market=US&limit=5"
-headers = {
-    "Authorization": "Bearer " + token
-}
+    # Step 3 - Use Token and playlist ID to return track information
 
-res = requests.get(url=playlistUrl, headers=headers)
+    playlistUrl = f"https://api.spotify.com/v1/playlists/{playlistId}/tracks?market=US&limit=10"
+    headers = {
+        "Authorization": "Bearer " + token
+    }
 
-for i in range(3):
-    artist = res.json()['items'][i]['track']['album']['artists'][0]['name']
-    title = res.json()['items'][i]['track']['album']['name']
-    image = res.json()['items'][i]['track']['album']['images'][0]['url']
+    res = requests.get(url=playlistUrl, headers=headers)
+
+    artist = res.json()['items'][rand_song]['track']['album']['artists'][0]['name']
+    title = res.json()['items'][rand_song]['track']['name']
+    image = res.json()['items'][rand_song]['track']['album']['images'][0]['url']
+    song_url = res.json()['items'][rand_song]['track']['external_urls']['spotify']
+    duration = (res.json()['items'][rand_song]['track']['duration_ms'])
 
     print(title)
     print(artist)
     print(image)
+    print(duration)
+    print(song_url)
     print('\n')
+    
+    # print(json.dumps(res.json(), indent=2))
 
-
-
-# print(json.dumps(res.json(), indent=2))
 
 
